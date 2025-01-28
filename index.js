a091a9aed6399b4ecd18311f98b6731181aac22d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-01-28 16:28:19 +0100

    added headers: { "Type-Content": "application/json"}

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ document.getElementById("blog-form").addEventListener("submit", function(e) {
     // blogPost.value = ""
 
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "POST", 
+        headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
             title: title.value,
             blogPost: blogPost.value
