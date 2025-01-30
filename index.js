dd33ebc76bf3a71a7fd0e7f1d96289c65b0ffdb9
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-01-30 10:46:09 +0100

    mapping over an array with data. Converting into string only in DOM innerHTML, the same repeats in second fetch where instead of mapping I unshift it but the same join("") position in innerHTML only not modifying the array

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 
 const postsDiv = document.getElementById("posts-div")
+let returnedHtml = []
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const dataArrReduced = data
+        const dataArrReduced = data.slice(0,2)
         console.log(dataArrReduced)
         
-        const returnedHtml = dataArrReduced.map(onePost => {
+         returnedHtml = dataArrReduced.map(onePost => {
             return `<h1>${onePost.title}</h1>
                     <p>${onePost.body}</p>
                     <hr/>
                     `
-        }).join("")
+        })
         
-        postsDiv.innerHTML = returnedHtml
+        console.log("returnedHtm", returnedHtml)
+        console.log("typeof", typeof returnedHtml)
+        postsDiv.innerHTML = returnedHtml.join("")
     })
 
 document.getElementById("blog-form").addEventListener("submit", function(e) {
@@ -22,24 +25,25 @@ document.getElementById("blog-form").addEventListener("submit", function(e) {
 
     let title = document.getElementById("title")
     let blogPost = document.getElementById("blog-post")
-    // const userPost = {
-    //     title: title.value,
-    //     blogPost: blogPost.value
-    // }
-    // console.log(userPost)
-    // title.value = ""
-    // blogPost.value = ""
 
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", { method: "POST", 
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
             title: title.value,
-            blogPost: blogPost.value
+            body: blogPost.value
         })
     })
     .then(res => res.json())
-    .then(data => {console.log(data)})
+    .then(data => {console.log(data)
 
+    returnedHtml.unshift(`<h1>${data.title}</h1>
+                    <p>${data.body}</p>
+                    <hr/>
+                    `)
+
+    postsDiv.innerHTML = returnedHtml.join("")
     title.value = ""
     blogPost.value = ""
+    })
+    .catch(error => console.error("Error:", error));
 })
